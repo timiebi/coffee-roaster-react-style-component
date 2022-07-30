2aaca21c3fad4e4ca70ab4ea2a79c99d217372f4
kosu timiebi nicholas <kosutimiebinicholas@gmail.com>
2022-07-30 09:06:24 +0100

    Add files via upload
    this is the setup i will use for this project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Routes,  Route, Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
